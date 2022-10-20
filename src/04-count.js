5ee7e8a9be7c5fcb93743c5c74e467bb1d6778f3
amontalvof <amontalvof92@gmail.com>
2022-10-20 09:40:35 -0500

    feat: count methods

--- src/04-count.js
+++ src/04-count.js
@@ -4,3 +4,17 @@
  */
 
 db.persons.aggregate([{ $count: 'allDocumentsCount' }]); // { "allDocumentsCount": 1000 }
+
+// Different count methods
+
+db.persons.aggregate([]).toArray().length;
+//1,7 Sec -> 1000 documents
+
+db.persons.aggregate([]).itcount();
+// 1,4 Sec -> 1000 documents
+
+db.persons.aggregate([{ $count: 'total' }]);
+// 0,21 Sec -> { "total" : 1000 }
+
+db.persons.find({}).count();
+// 0,21 Sec -> 1000
