1aaceeba64d4b9f84a5288a5f4f6a02ce83c0fe0
amontalvof <amontalvof92@gmail.com>
2022-11-04 07:53:20 -0600

    feat: other project example

--- src/08-project.js
+++ src/08-project.js
@@ -8,3 +8,17 @@ db.persons.aggregate([{ $project: { _id: 0, name: 1, age: 1 } }]); // 0 exclude 
 db.persons.aggregate([{ $project: { eyeColor: 0, age: 0 } }]);
 
 db.persons.aggregate([{ $project: { name: 1, newAge: '$age' } }]); // newAge is a new field that accepts an expression that in this case is age
+
+db.persons.aggregate([
+    { $project: { name: 1, 'company.location.country': 1 } },
+]);
+// result:
+// {
+//     "_id" : ObjectId("5ad4cbde2edbf6ddeec71741"),
+//     "name": "Aurelia Gonzales",
+//     "company": {
+//         "location": {
+//             "country": "USA"
+//         }
+//     }
+// }
