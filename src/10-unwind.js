3a1e5909ad6ee46f764b5e548f93861914a4e86c
amontalvof <amontalvof92@gmail.com>
2022-11-12 19:48:56 -0600

    feat: other unwind example

--- src/10-unwind.js
+++ src/10-unwind.js
@@ -6,3 +6,12 @@ db.persons.aggregate([
     { $unwind: '$tags' },
     { $project: { name: 1, gender: 1, tags: 1 } },
 ]);
+
+db.persons.aggregate([{ $unwind: '$tags' }, { $group: { _id: '$tags' } }]);
+// result:
+// {"_id" : "nulla" }
+// {"_id" : "reprehenderit" }
+// {"_id": "laboris" }
+// {"_id" : "anim" }
+// {"_id" : "consectetur" }
+// {"_id" : "sit" }
