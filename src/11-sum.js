e9342437b0e760a5a4e770da084475322b4fa471
amontalvof <amontalvof92@gmail.com>
2022-11-14 08:26:25 -0600

    feat: sum, unwind and group

--- src/11-sum.js
+++ src/11-sum.js
@@ -10,7 +10,6 @@ db.persons.aggregate([
         },
     },
 ]);
-
 // result:
 // {"_id": 27, "count": 42 }
 // {"_id": 30, "count": 38 }
@@ -19,3 +18,20 @@ db.persons.aggregate([
 // {"_id": 23, "count": 57 }
 // {"_id": 37, "count": 49 }
 // {"_id": 32, "count": 38 }
+
+db.persons.aggregate([
+    { $unwind: '$tags' },
+    {
+        $group: {
+            _id: '$tags',
+            count: { $sum: 1 },
+        },
+    },
+]);
+// result:
+// {"_id": "nulla", "count": 65 }
+// {"_id": "reprehenderit", "count": 57 }
+// {"_id": "laboris", "count" : 52 }
+// {"_id": "anim", "count": 35 }
+// { _id": "consectetur", "count": 62 }
+// {"_id": "sit", "count": 56 }
